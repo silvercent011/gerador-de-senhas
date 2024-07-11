83bda5a23dec9396396dd99cfe80cfa3f3aa26f5
Sidney Alex <38598808+silvercent011@users.noreply.github.com>
2024-07-11 12:06:55 -0300

    feat:backend: update controller

--- backend/src/main/java/com/backend/controller/PasswordController.java
+++ backend/src/main/java/com/backend/controller/PasswordController.java
@@ -1,22 +1,41 @@
 package com.backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.dto.PasswordRequest;
+import com.backend.model.Password;
+import com.backend.service.PasswordService;
 
 @RestController
 @RequestMapping("/api")
 public class PasswordController {
 
+    @Autowired
+    private PasswordService passwordService;
+
     @PostMapping("/generate-password")
-    public String generatePassword() {
-        return "password";
+    public Password generatePassword(
+            @RequestBody PasswordRequest passwordRequest) {
+
+        Password generatedPassword = passwordService.generatePassword(
+                passwordRequest.getLength(),
+                passwordRequest.isUseUpper(),
+                passwordRequest.isUseLower(),
+                passwordRequest.isUseNumbers(),
+                passwordRequest.isUseSpecial());
+
+        return generatedPassword;
     }
 
     @GetMapping("/password-history")
-    public String getPasswordHistory() {
-        return "password";
+    public Iterable<Password> getPasswordHistory() {
+
+        return passwordService.getPasswordHistory();
     }
 
 }
