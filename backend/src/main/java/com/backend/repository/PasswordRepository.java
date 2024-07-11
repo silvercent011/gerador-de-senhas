f13f46ce5ac4dc9d40d00b60d86c476aba245327
Sidney Alex <38598808+silvercent011@users.noreply.github.com>
2024-07-11 12:06:14 -0300

    feat:backend: create repository

---
+++ backend/src/main/java/com/backend/repository/PasswordRepository.java
@@ -0,0 +1,9 @@
+package com.backend.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.backend.model.Password;
+
+public interface PasswordRepository extends CrudRepository<Password, String> {
+
+}
