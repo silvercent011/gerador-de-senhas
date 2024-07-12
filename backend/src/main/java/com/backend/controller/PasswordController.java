9b59434465840c2b1a06633c7aa67af8c55da1b1
Sidney Alex <38598808+silvercent011@users.noreply.github.com>
2024-07-12 00:23:51 -0300

    feat:backend: update cors

--- backend/src/main/java/com/backend/controller/PasswordController.java
+++ backend/src/main/java/com/backend/controller/PasswordController.java
@@ -1,6 +1,7 @@
 package com.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import com.backend.dto.PasswordRequest;
 import com.backend.model.Password;
 import com.backend.service.PasswordService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class PasswordController {
