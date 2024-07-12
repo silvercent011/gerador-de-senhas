4cd5f66ba12e3026ac358bcba56a52bf2045d8d9
Sidney Alex <38598808+silvercent011@users.noreply.github.com>
2024-07-12 00:26:09 -0300

    feat:frontend show history on home page

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,20 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { ModalController } from '@ionic/angular';
 import { CreateFormComponent } from '../create-form/create-form.component';
+import { PasswordService } from '../services/password.service';
+import { Password } from '../types/Password';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  constructor(private modalController: ModalController) {}
+export class HomePage implements OnInit {
+  generatedPasswords: Password[] | undefined;
+
+  constructor(
+    private modalController: ModalController,
+    private passwordService: PasswordService
+  ) {}
+
+  ngOnInit(): void {
+    this.passwordService.passwordHistory().subscribe((data) => {
+      this.generatedPasswords = data;
+    });
+  }
 
   async openModal() {
     const modal = await this.modalController.create({
       component: CreateFormComponent,
     });
     modal.present();
+
+    await modal.onWillDismiss().then(() => {
+      this.ngOnInit();
+    });
   }
 }
