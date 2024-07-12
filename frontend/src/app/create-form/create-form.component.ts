4505f4b20ff6a3476a31ebf394a6d45eaab75ef1
Sidney Alex <38598808+silvercent011@users.noreply.github.com>
2024-07-12 00:26:26 -0300

    feat:frontend: update form

--- frontend/src/app/create-form/create-form.component.ts
+++ frontend/src/app/create-form/create-form.component.ts
@@ -1,18 +1,51 @@
 import { Component } from '@angular/core';
 
 import { ModalController } from '@ionic/angular';
+import { PasswordService } from '../services/password.service';
 @Component({
   selector: 'app-create-form',
   templateUrl: './create-form.component.html',
   styleUrls: ['./create-form.component.scss'],
 })
 export class CreateFormComponent {
-  name!: string;
+  length: number = 12;
+  useUpper: boolean = true;
+  useLower: boolean = true;
+  useNumbers: boolean = true;
+  useSpecial: boolean = true;
 
-  constructor(private modalCtrl: ModalController) {}
+  generatedPassword: string | undefined;
+
+  constructor(
+    private modalCtrl: ModalController,
+    private passwordService: PasswordService
+  ) {}
 
   confirm() {
-    return this.modalCtrl.dismiss({ name: this.name }, 'confirm');
+    return this.modalCtrl.dismiss(
+      {
+        length: this.length,
+        useUpper: this.useUpper,
+        useLower: this.useLower,
+        useNumbers: this.useNumbers,
+        useSpecial: this.useSpecial,
+      },
+      'confirm'
+    );
+  }
+
+  generatePassword() {
+    this.passwordService
+      .generatePassword({
+        length: this.length,
+        useUpper: this.useUpper,
+        useLower: this.useLower,
+        useNumbers: this.useNumbers,
+        useSpecial: this.useSpecial,
+      })
+      .subscribe((data) => {
+        this.generatedPassword = data.password;
+      });
   }
 
   cancel() {
